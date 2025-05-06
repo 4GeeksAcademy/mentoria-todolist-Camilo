005a51e0141d1538ba2cc380f076c2c45c13a209
Carlos Atanes Vences <carlos117g@gmail.com>
2025-05-06 16:59:21 +0000

    Añadido Delete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
 	const [tarea, setTarea] = useState("")
 	const [listaTareas, setListaTareas] = useState([])
+	// Para pasarlo a API leer la documentación
+	// https://playground.4geeks.com/todo/docs
 
 
 	function handleOnSubmit(event) {
 		event.preventDefault()
-		setListaTareas([...listaTareas, tarea])
+		setListaTareas([...listaTareas, tarea]) // Cuando tu vas a guardar algo -> POST
 		setTarea("")
 	}
+	
+	function borrar(i) {
+        const newList = [...listaTareas];
+        newList.splice(i, 1);
+        setListaTareas(newList); // Cuando tu vas a borrar algo -> DELETE
+	}
+
+	// useEffect y dentro el get 👀 
 
 	return (
 		<>
-			<form onSubmit={(event) => handleOnSubmit(event)}>
+			<form onSubmit={(event) => handleOnSubmit(event)}> {/* Cuando tu vas a guardar algo -> POST */}
 				<input placeholder="Introduce tu tarea" value={tarea} onChange={(event) => setTarea(event.target.value)}/>
 			</form>
 			<ol>
-				{(listaTareas.length == 1) ?
+				{(listaTareas.length == 1) ? // Cuando obtienes algo -> GET
 				listaTareas.map((elemento_a_mostrar, posicion_en_la_lista) => (
 					<li key={posicion_en_la_lista}>
 						{elemento_a_mostrar}
+						<i onClick={(posicion_en_la_lista) => borrar(posicion_en_la_lista)}>basura</i>
 					</li>
 				))
 				:
